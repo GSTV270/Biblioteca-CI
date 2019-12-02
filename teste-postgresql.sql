1dde7a27615467c2cfa42d4e94bcb31fbbdd5b3e
Igor Pereira <igorp9971@gmail.com>
2019-12-01 23:41:04 -0300

    Criação Triggers pgsql

--- teste-postgresql.sql
+++ teste-postgresql.sql
@@ -1,3 +1,4 @@
+--TIPO
 CREATE TYPE acesso_nested AS (
     senha VARCHAR(20)
 );
@@ -9,6 +10,7 @@ CREATE TYPE endereco_nested AS (
     cep INTEGER
 );
 
+--TABELA
 CREATE TABLE pessoa (
     cpf VARCHAR(11) PRIMARY KEY,
     nome VARCHAR(50),
@@ -47,3 +49,29 @@ CREATE TABLE devolucao (
     emprestimo INTEGER REFERENCES emprestimo(id) ON DELETE NO ACTION,
     devolucao DATE
 );
+
+--TRIGGER & FUNCAO
+CREATE OR REPLACE FUNCTION devolucao_BFINS() RETURNS TRIGGER AS $devolucao_bfins$
+	BEGIN
+        	UPDATE livro SET alugado = CAST(alugado AS SIGNED) - 1 WHERE id = (SELECT livro from emprestimo where id = NEW.id);
+	END;
+$devolucao_bfins$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION emprestimo_BFINS() RETURNS TRIGGER AS $emprestimo_bfins$
+    BEGIN
+        IF (SELECT qtde FROM livro WHERE id=NEW.livro) > (SELECT alugado FROM livro WHERE id=NEW.livro) AND (SELECT qtde FROM livro WHERE id=NEW.livro) > 0
+            THEN UPDATE livro SET alugado = alugado+1 WHERE id = NEW.livro;
+        ELSE
+            RAISE EXCEPTION 'O LIVRO NÃO PODE SER EMPRESTADO!';
+        END IF;
+    END;
+$emprestimo_bfins$ LANGUAGE plpgsql;
+
+CREATE TRIGGER devolucao_BEFORE_INSERT 
+    BEFORE INSERT ON devolucao
+    FOR EACH ROW EXECUTE PROCEDURE devolucao_BFINS();
+
+CREATE TRIGGER emprestimo_BEFORE_INSERT
+    BEFORE INSERT ON emprestimo
+    FOR EACH ROW EXECUTE PROCEDURE emprestimo_BFINS();
+    
